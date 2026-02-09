ea60e486dd7125e1e0686e77afd8bfcd490d7946
Enes Schatten Ozkan <enescakirfx@gmail.com>
2026-02-09 21:32:13 +0300

    Add countdown command for 24-hour timer

---
+++ src/commands/syc.js
@@ -0,0 +1,28 @@
+const discord = require("discord.js")
+
+module.exports = {
+    name: "durum",
+    description: "24 saatlik geri sayım başlatır ve bitince haber verir.",
+    usage: "!durum",
+    onlyowner: false,
+
+    run: async (client, message, args) => {
+        const now = Math.floor(Date.now() / 1000);
+
+        const duration = 86400;
+        const finishTime = now + duration;
+
+        await message.reply(
+            `${durum}\n\n` +
+            `⏳ **Açılış için Geri Sayım Başladı!**\n` +
+            `<a:arrow:1457356128026362078> Kalan Süre: <t:${finishTime}:R> ||@everyone||`
+        );
+
+        setTimeout(async () => {
+            message.channel.send(
+                `🎉 **Midgard Network açıldı!**\n` +
+                `İyi oyunlar dileriz ❤️ ||@everyone||`
+            );
+        }, duration * 1000); 
+    }
+};
