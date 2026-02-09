4fdfe3cf20d1060d9d71f7649b06d43584fd9d3c
Enes Schatten Ozkan <enescakirfx@gmail.com>
2026-02-09 21:12:31 +0300

    Update and rename ready.js to src/events/ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-    let activities = [ `Kayıt Botu` ], i = 0;
+    let activities = [ `ff` ], i = 0;
     setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
 }};
+
